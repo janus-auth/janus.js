9bbd1793be76f06a242b007e0cfacca8029c6a13
espoal <alberto.websci@gmail.com>
2023-05-11 22:15:28 +0200

    chore: cleanup

--- service/routes.ts
+++
@@ -1,6 +0,0 @@
-import { healthRoute } from '@pkgs/health'
-import { routerFactory, notFoundRoute } from '@libs/router'
-
-const routes = [healthRoute]
-
-export const router = routerFactory(routes, notFoundRoute)
